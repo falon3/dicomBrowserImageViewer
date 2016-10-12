f3f5d38f1e7319e389c47a0f97315d1543e951b9
Falon <scheers@ualberta.ca>
2016-10-12 14:50:16 -0600

    started project, made basic framework and webpage for requests/uploads. Runs locally only right now

---
+++ app.py
@@ -0,0 +1,27 @@
+#!flask/bin/python
+from flask import Flask, abort, send_file, render_template, request, url_for
+
+app = Flask(__name__)
+
+@app.route('/api/')
+def index():
+    return render_template('upload.html')
+
+@app.route('/api/upload/', methods=['POST'])
+def upload():
+    #pass (upload and convert dicom)
+    return render_template('submit.html')
+
+@app.route('/api/upload/<int:img_id>', methods=['GET'])
+def get_image(img_id):
+    try:
+       filename = 'images/IM-0001-'+ str(img_id).zfill(4) + '.jpg'
+       return send_file(filename)
+    except:
+       filename = 'images/error.gif'
+       return send_file(filename)
+
+if __name__ == '__main__':
+    host="localhost",
+    port=int("8080")
+    app.run(debug=True)
