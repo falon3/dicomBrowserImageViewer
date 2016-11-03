b563ddf1252b4e769d5a295224fc4c559acaa966
David Turner <finalfantasykid13@gmail.com>
2016-11-03 14:33:48 -0600

    -Turned the viewer into a BackboneView

--- static/scripts/viewer.js
+++ static/scripts/viewer.js
@@ -1,255 +1,266 @@
-RADIUS = 2.5;
+Viewer = Backbone.View.extend({
 
-$(document).ready(function(){
+    RADIUS: 2.5,
+    SEGMENTS: 18,
+    
+    images: new Images(),
+    mousePoint: {x: -1000, y: -1000},
+    closeLine: null,
+    closePoint: null,
+    dragging: false,
+    segments: this.SEGMENTS,
+    naturalWidth: 1,
+    natrualHeight: 1,
+    scalingFactor: 1,
+    slice: null,
+    canvas: null,
+    context: null,
 
-    $("#curved").prop("checked", true);
-    if($("#preCachedImages img").length == 0){
-        return;
-    }
-
-    var images = new Images();
-    var mousePoint = {x: 0, y: 0};
-    var closePoint = null;
-    var closeLine = null;
-    var dragging = false;
-    var segments = 18;
+    initialize: function(){
+        _.each($("#preCachedImages img"), $.proxy(function(img, i){
+            var image = new Image({src: $(img).attr('src')});
+            this.images.add(image);
+        }, this));
+        this.slice = this.$("#slice");
+        this.canvas = this.$("canvas")[0];
+        this.context = this.canvas.getContext('2d');
+        this.$("#curved").prop("checked", true);
+        this.renderUI();
+        this.render();
+        
+        /* Some event listeners */
+        this.$("#canvas").bind('mousewheel DOMMouseScroll', $.proxy(this.scroll, this));
+        $(window).bind('keyup', $.proxy(function(e){ if(e.keyCode == 46){ this.deletePoint(); }}, this)),
+        $(window).resize($.proxy(this.render, this));
+    },
     
-    var naturalWidth = 1;
-    var naturalHeight = 1;
+    // Deletes the selected point
+    deletePoint: function(){
+        this.closeLine.getPoints().remove(this.closePoint);
+        this.closePoint = null;
+        this.render();
+    },
     
-    var scalingFactor = 1;
-
-    var canvas = document.getElementById('canvas');
-    var context = canvas.getContext('2d');
-    
-    // Initialize the points (this will probably be done via an api call eventually)
-    _.each($("#preCachedImages img"), function(img, i){
-        var image = new Image({src: $(img).attr('src')});
-        images.add(image);
-    });
-    
-    var deleteSelectedPoint = function(){
-        closeLine.getPoints().remove(closePoint);
-        closePoint = null;
-        render();
-    };
-    
-    // The main render function for the viewer.  
-    // It is called everytime that a change has been made
-    var render = function(){
-        // Changing the image
-        $("#slice").attr("src", images.getCurrentImage().get('src'));
-        $("#currentImage").text(images.currentId+1);
-        $("#maxImage").text(images.length);
-        
-        // Canvas
-        naturalWidth = $("#slice")[0].naturalWidth;
-        naturalHeight = $("#slice")[0].naturalHeight;
-        
-        scalingFactor = naturalWidth/$("#slice").width();
-        
-        canvas.width = $("#slice").width();
-        canvas.height = $("#slice").height();
-        
-        if(canvas.width == 0){
-            // Image wasn't done loading yet, try again later...
-            _.defer(render);
-            return;
-        }
-        
-        if(!dragging){
-            closePoint = null;
-            closeLine = null;
-        }
-        
-        images.getCurrentImage().getLines().each(function(line, i){
-            var curvePoints = new Array();
-            line.getPoints().each(function(point, j){
-                curvePoints.push(point.get('x')/scalingFactor);
-                curvePoints.push(point.get('y')/scalingFactor);
-                // While we are here, check distance to points
-                if(!dragging && euclideanDistance(point.getCoords(), mousePoint) <= RADIUS*1.75){
-                    closePoint = point;
-                    closeLine = line;
-                }
-            });
-            
-            context.drawCurve(curvePoints, 0.5, false, segments);
-            context.strokeStyle = 'red';
-            if(line == images.getCurrentImage().getCurrentLine()){
-                context.lineWidth = 2/scalingFactor;
-            }
-            else{
-                context.lineWidth = 1/scalingFactor;
-            }
-            context.stroke();
-            line.getPoints().each(function(point, j){
-                var radius = RADIUS/scalingFactor;
-                if(point == closePoint && line == closeLine){
-                    radius *= 1.75;
-                }
-                context.beginPath();
-                context.arc(point.get('x')/scalingFactor, point.get('y')/scalingFactor, radius, 0, 2 * Math.PI, false);
-                context.fillStyle = 'red';
-                context.fill();
-            });
-        });
-        
-        context.beginPath();
-        context.arc(mousePoint.x/scalingFactor, mousePoint.y/scalingFactor, RADIUS/scalingFactor, 0, 2 * Math.PI, false);
-        context.fillStyle = 'red';
-        context.fill();
-        
-        // Updating button appearance
-        if(images.isFirst()){
-            $("#previous").prop('disabled', true);
+    // Toggles whether or not to have curved lines
+    changeSegments: function(){
+        if(this.$("#curved").is(":checked")){
+            this.segments = this.SEGMENTS;
         }
         else{
-            $("#previous").prop('disabled', false);
+            this.segments = 1;
         }
-        if(images.isLast()){
-            $("#next").prop('disabled', true);
-        }
-        else{
-            $("#next").prop('disabled', false);
-        }
-    };
+        this.render();
+    },
     
-    /* Event Listeners */
+    // Display the previous image
+    previous: function(e){
+        this.images.previous();
+        this.renderUI();
+        this.render();
+    },
     
-    $(window).resize(render);
+    // Display the next image
+    next: function(e){
+        this.images.next();
+        this.renderUI();
+        this.render();
+    },
     
-    // Clicking the Previous button
-    $("#previous").click(function(){
-        images.previous();
-        render();
-    });
-    
-    // Clicking the Next button
-    $("#next").click(function(){
-        images.next();
-        render();
-    });
-    
-    // Mouse wheen scrolling
-    $("#canvas").bind('mousewheel DOMMouseScroll', function(e){
+    // Scroll forward/back the list of images
+    scroll: function(e){
         if (e.originalEvent.wheelDelta > 0 || e.originalEvent.detail < 0) {
             // Go to the previous image
-            if(!images.isFirst()){
-                images.previous();
-                render();
+            if(!this.images.isFirst()){
+                this.previous();
             }
         }
         else {
             // Go to the next image
-            if(!images.isLast()){
-                images.next();
-                render();
+            if(!this.images.isLast()){
+                this.next();
             }
         }
         e.preventDefault();
-    });
+    },
     
-    // Clicking the curved button
-    $("#curved").change(function(e){
-        if($("#curved").is(":checked")){
-            segments = 18;
+    // Toggles whether or not to go full screen
+    toggleFullScreen: function(){
+        this.$("#viewerContainer").toggleClass("fullscreen");
+        if(this.$("#viewerContainer").hasClass("fullscreen")){
+            this.$("#fullscreen img").attr("src", "/static/exitFullScreen.png");
+            this.$("#fullscreen img").attr("alt", "Exit Full Screen");
+            this.$("#fullscreen img").attr("title", "Exit Full Screen");
         }
         else{
-            segments = 1;
+            this.$("#fullscreen img").attr("src", "/static/fullScreen.png");
+            this.$("#fullscreen img").attr("alt", "Full Screen");
+            this.$("#fullscreen img").attr("title", "Full Screen");
         }
-        render();
-    });
+        this.render();
+    },
     
-    // Clicking the full screen button
-    $("#fullscreen a").click(function(e){
-        $("#viewerContainer").toggleClass("fullscreen");
-        if($("#viewerContainer").hasClass("fullscreen")){
-            $("#fullscreen img").attr("src", "/static/exitFullScreen.png");
-            $("#fullscreen img").attr("alt", "Exit Full Screen");
-            $("#fullscreen img").attr("title", "Exit Full Screen");
-        }
-        else{
-            $("#fullscreen img").attr("src", "/static/fullScreen.png");
-            $("#fullscreen img").attr("alt", "Full Screen");
-            $("#fullscreen img").attr("title", "Full Screen");
-        }
-        render();
-    });
-    
-    // Pressing the delete key
-    $(window).keyup(function(e){
-        if(e.keyCode == 46){ // Delete
-            deleteSelectedPoint();
-        }
-    });
-    
-    // Disable the context menu
-    canvas.addEventListener('contextmenu', function(e){
-        e.preventDefault();
-    });
-    
-    // Mouse moving
-    canvas.addEventListener('mousemove', function(e){
-        coords = canvas.relMouseCoords(e);
-        coords.x *= scalingFactor;
-        coords.y *= scalingFactor;
-        mousePoint = coords;
-        if(dragging){
-            if(closeLine == null){
-                closeLine = images.getCurrentImage().getCurrentLine();
+    // Moving the mouse coordinates
+    mouseMove: function(e){
+        coords = this.canvas.relMouseCoords(e);
+        coords.x *= this.scalingFactor;
+        coords.y *= this.scalingFactor;
+        this.mousePoint = coords;
+        if(this.dragging){
+            if(this.closeLine == null){
+                this.closeLine = this.images.getCurrentImage().getCurrentLine();
             }
-            closePoint.setCoords(mousePoint);
+            this.closePoint.setCoords(this.mousePoint);
         }
-        render();
-    });
+        this.render();
+    },
     
-    // Clicking to add a point
-    canvas.addEventListener('mousedown', function(e){
-        coords = canvas.relMouseCoords(e);
-        coords.x *= scalingFactor;
-        coords.y *= scalingFactor;
+    // Clicking the mouse button (both left/right)
+    mouseDown: function(e){
+        coords = this.canvas.relMouseCoords(e);
+        coords.x *= this.scalingFactor;
+        coords.y *= this.scalingFactor;
         if(e.button == 2){
             // Right Click
-            if(closePoint != null){
-                deleteSelectedPoint();
+            if(this.closePoint != null){
+                this.deletePoint();
                 return;
             }
             else {
-                images.getCurrentImage().getLines().add(new Line());
+                this.images.getCurrentImage().getLines().add(new Line());
             }
         }
         if(e.button == 0 || e.button == 2){
             // Left Click (or Right Click)
-            dragging = true;
-            if(closePoint == null){
+            this.dragging = true;
+            if(this.closePoint == null){
                 // Adding new point
-                if(images.getCurrentImage().getCurrentLine() == null){
-                    images.getCurrentImage().getLines().add(new Line());
+                if(this.images.getCurrentImage().getCurrentLine() == null){
+                    this.images.getCurrentImage().getLines().add(new Line());
                 }
-                closePoint = new Point({x: coords.x, y: coords.y});
-                images.getCurrentImage().getCurrentLine().getPoints().add(closePoint);
+                this.closePoint = new Point({x: coords.x, y: coords.y});
+                this.images.getCurrentImage().getCurrentLine().getPoints().add(this.closePoint);
             }
             else{
-                images.getCurrentImage().setCurrentLine(closeLine);
+                this.images.getCurrentImage().setCurrentLine(this.closeLine);
             }
         }
-        render();
-    });
+        this.render();
+    },
     
-    // Leaving the canvas
-    canvas.addEventListener('mouseleave', function(e){
-        dragging = false;
-        mousePoint = {x: -1000, y: -1000};
-        render();
-    });
+    // Leaving the canvas area
+    mouseLeave: function(e){
+        this.dragging = false;
+        this.mousePoint = {x: -1000, y: -1000};
+        this.render();
+    },
     
-    // Releasing the mouse
-    canvas.addEventListener('mouseup', function(e){
-        dragging = false;
-        render();
-    });
+    // Releasing the mouse button
+    mouseUp: function(e){
+        this.dragging = false;
+        this.render();
+    },
+    
+    events: {
+        "click #previous": "previous",
+        "click #next": "next",
+        "change #curved": "changeSegments",
+        "click #fullscreen a": "toggleFullScreen",
+        "contextmenu #canvas": function(e){ e.preventDefault(); },
+        "mousemove #canvas": "mouseMove",
+        "mousedown #canvas": "mouseDown",
+        "mouseleave #canvas": "mouseLeave",
+        "mouseup #canvas": "mouseUp"
+    },
+    
+    renderUI: function(){
+        // Changing the image
+        this.slice.attr("src", this.images.getCurrentImage().get('src'));
+        this.$("#currentImage").text(this.images.currentId+1);
+        this.$("#maxImage").text(this.images.length);
+        
+        // Updating button appearance
+        if(this.images.isFirst()){
+            this.$("#previous").prop('disabled', true);
+        }
+        else{
+            this.$("#previous").prop('disabled', false);
+        }
+        if(this.images.isLast()){
+            this.$("#next").prop('disabled', true);
+        }
+        else{
+            this.$("#next").prop('disabled', false);
+        }
+    },
 
-    render();
+    render: function(){
+        console.log("RENDER");
+        if(this.slice[0] == undefined){
+            // Something went wrong, try to re-get the slice
+            this.slice = this.$("#slice");
+        }
+        this.naturalWidth = this.slice[0].naturalWidth;
+        this.naturalHeight = this.slice[0].naturalHeight;
+        
+        this.scalingFactor = this.naturalWidth/this.slice.width();
+        
+        this.canvas.width = this.slice.width();
+        this.canvas.height = this.slice.height();
+        
+        if(this.canvas.width == 0){
+            // Image wasn't done loading yet, try again later...
+            _.defer(this.render);
+            return;
+        }
+        
+        if(!this.dragging){
+            this.closePoint = null;
+            this.closeLine = null;
+        }
+        
+        this.images.getCurrentImage().getLines().each(function(line, i){
+            var curvePoints = new Array();
+            line.getPoints().each(function(point, j){
+                curvePoints.push(point.get('x')/this.scalingFactor);
+                curvePoints.push(point.get('y')/this.scalingFactor);
+                // While we are here, check distance to points
+                if(!this.dragging && euclideanDistance(point.getCoords(), this.mousePoint) <= this.RADIUS*1.75){
+                    this.closePoint = point;
+                    this.closeLine = line;
+                }
+            }, this);
+            
+            this.context.drawCurve(curvePoints, 0.5, false, this.segments);
+            this.context.strokeStyle = line.get('color');
+            if(line == this.images.getCurrentImage().getCurrentLine()){
+                this.context.lineWidth = 2/this.scalingFactor;
+            }
+            else{
+                this.context.lineWidth = 1/this.scalingFactor;
+            }
+            this.context.stroke();
+            line.getPoints().each(function(point, j){
+                var radius = this.RADIUS/this.scalingFactor;
+                if(point == this.closePoint && line == this.closeLine){
+                    radius *= 1.75;
+                }
+                this.context.beginPath();
+                this.context.arc(point.get('x')/this.scalingFactor, point.get('y')/this.scalingFactor, radius, 0, 2 * Math.PI, false);
+                this.context.fillStyle = line.get('color');
+                this.context.fill();
+            }, this);
+        }, this);
+        
+        this.context.beginPath();
+        this.context.arc(this.mousePoint.x/this.scalingFactor, 
+                         this.mousePoint.y/this.scalingFactor, 
+                         this.RADIUS/this.scalingFactor, 0, 2 * Math.PI, false);
+        this.context.fillStyle = 'red';
+        this.context.fill();
+    }
 
+});
+
+$(document).ready(function(){
+    var viewer = new Viewer({el: $("#viewer")});
 });
